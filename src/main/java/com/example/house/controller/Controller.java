1aceb71e69302d426ea8db3b639dd812cf099675
XuRouqing <9577036+xu-rouqing@user.noreply.gitee.com>
2021-11-05 15:47:40 +0800

    spring

--- src/main/java/com/example/house/controller/Controller.java
+++ src/main/java/com/example/house/controller/Controller.java
@@ -6,11 +6,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpSession;
 
 @org.springframework.stereotype.Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class Controller {
     @RequestMapping("/index")
     public String testSession(HttpSession session) {
-        return "idnex";
+        return "index";
     }
 
+    @RequestMapping("/account")
+    public String toAccount() {
+        return "my-account";
+    }
+
+    @RequestMapping("/fullwidth")
+    public String tofullwidth() {
+        return "shop-grid-fullwidth";
+    }
+
+    @RequestMapping("/designer-list")
+    public String todesignerlist() {
+        return "designer-list.html";
+    }
+
+    @RequestMapping("/worker-list")
+    public String toworkderlist() {
+        return "worker-list.html";
+    }
+
+    @RequestMapping("/discount")
+    public String todiscount() {
+        return "discount.html";
+    }
+
+    @RequestMapping("/calendar")
+    public String tocalendar() {
+        return "calendar.html";
+    }
+
+    @RequestMapping("/contact")
+    public String tocontact() {
+        return "contact.html";
+    }
+
+    @RequestMapping("/login-register")
+    public String tologin() {
+        return "login-register.html";
+    }
+
+    @RequestMapping("/booking")
+    public String tobooking() {
+        return "booking.html";
+    }
 }
